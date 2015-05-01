d6925caca38bcf1455c367b4f045ff293840cb6a
andreas <andreas@hc-devel-xu12>
2015-05-01 05:22:22 +0200

    Initial Commit

---
+++ lib/default_bootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Copyright (c) 2015, Andreas Prucha, Helicon Software Development
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+if (!defined('HCYII2_PHPUNIT_VENDOR_DIR')) {
+    if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+        define('HCYII2_PHPUNIT_VENDOR_DIR', __DIR__ . '/../../../vendor');
+    }
+    elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+        define('HCYII2_PHPUNIT_VENDOR_DIR', __DIR__ . '/../vendor');
+    }
+}
+
+if (!defined('HCYII2_PHPUNIT_VENDOR_DIR'))
+{
+    throw new \Exception('Vendor dir not found');
+}
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+
+require_once(HCYII2_PHPUNIT_VENDOR_DIR . '/autoload.php');
+require_once(HCYII2_PHPUNIT_VENDOR_DIR . '/yiisoft/yii2/Yii.php');
+
+\Yii::setAlias('@helicon\\hcyii2\\phpunit', dirname(__DIR__));
+if (defined('HCYII2_PHPUNIT_TEST_NAMESPACE') &&
+        (!\Yii::getAlias('@' . HCYII2_PHPUNIT_TEST_NAMESPACE, false))) {
+    \Yii::setAlias('@' . HCYII2_PHPUNIT_TEST_NAMESPACE, HCYII2_PHPUNIT_TEST_DIR);
+}
